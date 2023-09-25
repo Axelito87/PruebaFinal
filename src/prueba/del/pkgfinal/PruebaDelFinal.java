6c7facbfb65aab89f9fc911812050269da5f37c4
Axelito87 <146007469+Axelito87@users.noreply.github.com>
2023-09-25 11:05:43 -0500

    Update PruebaDelFinal.java

--- src/prueba/del/pkgfinal/PruebaDelFinal.java
+++ src/prueba/del/pkgfinal/PruebaDelFinal.java
@@ -8,6 +8,6 @@ public class PruebaDelFinal {
     public static void main(String[] args) {
       System.out.println("Habla p causa GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
-        sout
+   
 
 }
