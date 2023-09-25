f54d46fcae2516a1b714160b299617967e13da25
LAB-USR-LN6377-A0607 <LAB-USR-LN6377-A0607@UTP362626.grupoutp.net>
2023-09-25 10:52:29 -0500

    pRUEBA FINAL

---
+++ src/prueba/del/pkgfinal/PruebaDelFinal.java
@@ -0,0 +1,12 @@
+
+package prueba.del.pkgfinal;
+
+
+public class PruebaDelFinal {
+
+   
+    public static void main(String[] args) {
+      System.out.println("Habla p causa GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+    
+}
